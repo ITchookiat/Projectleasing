776acca5a86ea5646dfb194dd35c4677d68aec02
hakim <it.mazda.chookiat@gmail.com>
2020-02-15 16:08:34 +0700

    edit holdcar

--- resources/views/precipitate/viewstock.blade.php
+++ resources/views/precipitate/viewstock.blade.php
@@ -110,8 +110,8 @@
                             <th class="text-center" width="70px">วันที่ยึด</th>
                             <th class="text-center">ทีมยึด</th>
                             <th class="text-center">ค่ายึด</th>
-                            <th class="text-center">รายละเอียด</th>
-                            <th class="text-center" width="120px">สถานะ</th>
+                            <th class="text-center" width="120px">รายละเอียด</th>
+                            <th class="text-center" width="90px">สถานะ</th>
                             <th class="text-center" width="100px">ตัวเลือก</th>
                           </tr>
                         </thead>
