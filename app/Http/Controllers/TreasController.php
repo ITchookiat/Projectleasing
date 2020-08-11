8cc64cf2663bb526ddce537d3dd3804ad594deb7
ITchookiat <50354446+ITchookiat@users.noreply.github.com>
2020-08-11 11:38:53 +0700

    Update TreasController.php

--- app/Http/Controllers/TreasController.php
+++ app/Http/Controllers/TreasController.php
@@ -103,7 +103,7 @@ class TreasController extends Controller
         elseif ($type == 3) {
             $data = DB::table('buyers')
                 ->leftJoin('cardetails','buyers.id','=','cardetails.Buyercar_id')
-                ->where('cardetails.Date_Appcar','=',date('Y-m-d'))
+                ->where('cardetails.Date_Appcar','>=',date('Y-m-d'))
                 ->where('cardetails.UserCheckAc_car','=',Null)
                 ->get();
                 $countData = Count($data);
