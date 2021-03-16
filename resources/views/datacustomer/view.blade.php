3be42112d97e061a9ed71c81731bbea1130b1327
ITchookiat <50354446+ITchookiat@users.noreply.github.com>
2021-03-16 15:52:22 +0700

    Update view.blade.php

--- resources/views/datacustomer/view.blade.php
+++ resources/views/datacustomer/view.blade.php
@@ -306,7 +306,7 @@
                     </div>
                   </div>
                 </div>
-                <hr>
+                {{--<hr>
                 <div class="row">
                   <div class="col-6">
                     <div class="form-group row mb-0">
@@ -348,6 +348,7 @@
                     </div>
                   </div>
                 </div>
+                --}}
                 <hr>
                 <div class="row">
                   <div class="col-6">
